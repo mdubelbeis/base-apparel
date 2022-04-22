f1f94c2461bf895a5024ddcc229ae6830515c4a8
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-22 02:20:03 -0400

    added additional form validation

--- src/components/Input.component.js
+++ src/components/Input.component.js
@@ -6,6 +6,7 @@ import errorIcon from '../images/icon-error.svg';
 const Input = () => {
   const [input, setInput] = useState('');
   const [error, setError] = useState('');
+  const [access, setAccess] = useState(false);
 
   const onFormSubmit = (e) => {
     e.preventDefault();
@@ -13,8 +14,10 @@ const Input = () => {
 
     if (!input.includes('@')) {
       setError('Please provide a valid email');
+      setAccess(false);
     } else {
-      setError('');
+      setError('Access Granted');
+      setAccess(true);
     }
   };
 
@@ -25,21 +28,29 @@ const Input = () => {
           <input
             value={input}
             onChange={(e) => setInput(e.target.value)}
-            className="border-2 border-desat-red opacity-50 pl-4 w-full py-2 rounded-full placeholder-desat-red"
+            className="focus:outline-soft-red border-2 border-desat-red opacity-50 pl-4 w-full py-2 rounded-full placeholder-desat-red"
             type="text"
             placeholder="Email Address"
           />
         </label>
-        <button className="absolute shadow-xl shadow-desat-red block right-[1px] bottom-[1px] px-[25px] py-[11px] rounded-full bg-gradient-to-br from-desat-pink to-soft-pink">
+        <button className="focus:outline-soft-red absolute shadow-xl shadow-desat-red block right-[1px] bottom-[1px] px-[25px] py-[11px] rounded-full bg-gradient-to-br from-desat-pink to-soft-pink">
           <img src={arrow} alt="arrow icon" />
         </button>
         {error && (
-          <span className="absolute left-4 -bottom-6 text-sm text-soft-red">
+          <span
+            className={`absolute left-4 -bottom-6 text-sm ${
+              access ? 'text-green-500' : 'text-red-500'
+            } `}
+          >
             {error}
           </span>
         )}
         {error && (
-          <div className="absolute right-16 mr-2 bottom-[10px] ">
+          <div
+            className={`absolute right-16 mr-2 bottom-[10px] ${
+              access ? 'hidden' : 'block'
+            }`}
+          >
             <img src={errorIcon} alt="Error icon" />
           </div>
         )}
