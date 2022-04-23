9833b02b1b0f986441f55ff3e2db33baa924e513
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-23 13:51:27 -0400

    added vertical spacing

--- src/components/Info.component.js
+++ src/components/Info.component.js
@@ -3,7 +3,7 @@ import Input from './Input.component';
 const Info = () => {
   return (
     <section className="text-center p-8 md:p-8 w-full lg:text-left lg:p-0 lg:max-w-[300px]">
-      <h1 className="tracking-[10.83px] text-[40px] space-y-2 mb-4 lg:text-[64px]">
+      <h1 className="tracking-[10.83px] text-[40px] space-y-6 mb-4 lg:text-[64px]">
         <span className="block text-desat-red font-light">WE'RE</span>
         <span className="block leading-[42px] lg:leading-[71px]">
           COMING SOON
