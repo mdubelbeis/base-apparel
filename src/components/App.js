44275e850dc281aa6fea867a4fea9efea1a94747
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-23 13:53:27 -0400

    added top padding to container

--- src/components/App.js
+++ src/components/App.js
@@ -7,7 +7,7 @@ function App() {
   return (
     <div>
       <div className="font-josefin-sans text-base mx-auto lg:mx-12 max-w-xl">
-        <div class="container flex flex-col lg:space-y-20 max-w-[600px] lg:pl-20 xl:pl-64 ">
+        <div class="container flex flex-col lg:space-y-20 max-w-[600px] lg:pl-20 lg:pt-10 xl:pl-64 xl:pt-10">
           <header className="p-8 lg:p-0 lg:mt-36">
             <img className="w-24 lg:w-48" src={Logo} alt="" />
           </header>
