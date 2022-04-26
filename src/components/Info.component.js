711fa127d9f5f2e958a14b3917b8b96d767b57d4
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-25 20:18:05 -0400

    fixed width issue

--- src/components/Info.component.js
+++ src/components/Info.component.js
@@ -2,10 +2,10 @@ import Input from './Input.component';
 
 const Info = () => {
   return (
-    <section className="text-center p-8 w-full md:px-32 lg:text-left lg:p-0 lg:max-w-[300px]">
-      <h1 className="tracking-[10.83px] text-[40px] space-y-6 mb-4 lg:text-[64px] lg:px-0">
+    <section className="text-center p-8 w-[375px] sm:p-4 sm:max-w-[500px] lg:text-left lg:p-0 lg:max-w-[400px]">
+      <h1 className="tracking-[10.83px] text-[40px] space-y-4 mb-4 md:mt-6 lg:text-[64px] lg:px-0">
         <span className="block text-desat-red font-light">WE'RE</span>
-        <span className="block leading-[42px] lg:leading-[71px]">
+        <span className="block leading-[42px] lg:leading-[60px]">
           COMING SOON
         </span>
       </h1>
